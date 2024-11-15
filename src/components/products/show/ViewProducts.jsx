a711455e18e98bb5fb79457f3116c208ff2e7a6d
Luis <alu.magallanes@gmail.com>
2024-11-15 12:53:00 -0300

    add folder css

--- src/components/products/show/ViewProducts.jsx
+++ src/components/products/show/ViewProducts.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useContext } from 'react';
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../../../firebase/firebaseconfig';
 import { CartContext } from '../../../context/dataContext';  // Asegúrate de que sea el contexto correcto
-import './viewproducts.css';
+import './css/viewproducts.css';
 import Swal from 'sweetalert2';
 
 const ViewProducts = () => {
